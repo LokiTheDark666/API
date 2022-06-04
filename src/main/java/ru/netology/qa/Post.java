9de2a5b9ee7e301aa09a06c18757786f94246f27
LokiTheDark666 <khcr.loki@mail.ru>
2022-06-04 15:54:31 +0300

    v1.0

---
+++ src/main/java/ru/netology/qa/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.qa;
+
+public class Post {
+    public String name;
+    public String surname;
+    public String patronymic;
+    public int passport;
+    public int phone;
+    public String subscription;
+
+}
